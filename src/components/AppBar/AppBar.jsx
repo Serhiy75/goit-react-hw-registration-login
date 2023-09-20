157594455b5cc4dfb32ee93ccf88b958198eddfc
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-20 14:43:09 +0300

    123

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,15 @@
+import { Navigation } from 'components/Navigation/Navigation';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { useAuth } from 'hooks/useAuth';
+import css from './AppBar.module.css';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+
+export const AppBar = () => {
+  const { isLoggedIn } = useAuth();
+  return (
+    <header className={css.header}>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </header>
+  );
+};
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { register, logIn, logOut, refreshUser } from './operations';
+
+const initialState = {
+  token: null,
+  isLggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLggedIn = true;
+    },
+    [logIn.fulfilled](state, action) {
+      state.usre = action.payload.user;
+      state.token = action.payload.token;
+      state.isLggedIn = true;
+    },
+    [logOut.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLggedIn = false;
+    },
+    [refreshUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [refreshUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLggedIn = true;
+      state.isRefreshing = false;
+    },
+    [refreshUser.rejected](state) {
+      state.isRefreshing = false;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
